d738e2dec0a6e0cc83cca3fd31ec3273a84251c2
Sadık Cihan Ayaz <scayaz.19.19@gmail.com>
2022-08-21 23:07:58 +0300

    feat: ButtonC is added.

---
+++ ShoppingListApp/components/ButtonC/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Text, Pressable} from 'react-native';
+import style from './style';
+
+interface Props {
+  title: string;
+  onPress(): void;
+}
+
+const ButtonC: React.FC<Props> = props => {
+  const {title, onPress} = props;
+  return (
+    <Pressable style={[style.container, style.center]} onPress={onPress}>
+      <Text style={style.text}>{title}</Text>
+    </Pressable>
+  );
+};
+
+export default ButtonC;
---
+++ ShoppingListApp/components/ButtonC/style.tsx
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#eb8634',
+    height: 40,
+    padding: 10,
+    borderWidth: 0,
+    borderRadius: 3,
+  },
+  center: {
+    alignContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#ffffff',
+  },
+});
